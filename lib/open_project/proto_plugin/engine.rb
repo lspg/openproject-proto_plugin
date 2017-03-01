5e6b8e1e976b2a7451b9e33daf12f1ec4ff0d588
Loïs PUIG <lois.puig@kctus.fr>
2017-03-01 15:21:54 +0100

    Clean code to keep css moding only

--- lib/open_project/proto_plugin/engine.rb
+++ lib/open_project/proto_plugin/engine.rb
@@ -8,6 +8,52 @@ module OpenProject::ProtoPlugin
 
     include OpenProject::Plugins::ActsAsOpEngine
 
-    assets %w(proto_plugin/main.css kitty.png lds/lds_logo.png lds/lds_logo_petit.png)
+    register(
+      'openproject-proto_plugin',
+      :author_url => 'https://openproject.org',
+      :requires_openproject => '>= 6.0.0'
+    ) do
+      # We define a new project module here for our controller including a permission.
+      # The permission is necessary for us to be able to add menu items to the project
+      # menu. You will not need to add a permission for adding menu items to the `top_menu`
+      # or `admin_menu`, however.
+      #
+      # You may have to enable the project module ("Kittens module") under project
+      # settings before you can see the menu entry.
+      project_module :kittens_module do
+        permission :view_kittens, { kittens: [:index] }
+      end
+
+      menu :project_menu,
+           :kittens,
+           { controller: '/kittens', action: 'index' },
+           after: :overview,
+           param: :project_id,
+           caption: "Kittens",
+           html: { class: 'icon2 icon-bug', id: "kittens-menu-item" },
+           if: ->(project) { true }
+    end
+
+    initializer 'proto_plugin.register_hooks' do
+      require 'open_project/proto_plugin/hooks'
+    end
+
+    initializer 'proto_plugin.homescreen_blocks' do
+      OpenProject::Static::Homescreen.manage :blocks do |blocks|
+        blocks.push(
+          { partial: 'homescreen_block', if: Proc.new { true } }
+        )
+      end
+    end
+
+    initializer 'proto_plugin.notifications' do
+      OpenProject::Notifications.subscribe 'user_invited' do |token|
+        user = token.user
+
+        Rails.logger.debug "#{user.mail} invited to OpenProject"
+      end
+    end
+
+    assets %w(proto_plugin/main.css lds/lds_logo.png lds/lds_logo_petit.png)
   end
 end
